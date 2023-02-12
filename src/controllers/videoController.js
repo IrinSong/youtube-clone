80e0f935b5c7fd14e51f0bec29063cfba0eade15
IrinSong <110530872+IrinSong@users.noreply.github.com>
2023-02-13 08:59:01 +0900

    add pug files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
-export const trending = (req, res) => res.send("Recommended Video");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
-export const watch = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => {
   console.log(req.params);
   return res.send("Edit Video");
